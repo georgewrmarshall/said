540592cc2b2b227a1dc4f5aa2091feb544cfb3f1
George Marshall <georgewrmarshall@gmail.com>
2020-11-01 21:07:26 -0800

    Updates

---
+++ component---src-pages-index-js-349bece098063304fd32.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{RXBc:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),i=n.n(a),c=n("Kvkj");t.default=function(e){var t=e.siteTitle;return i.a.createElement(c.d,null,i.a.createElement(c.f,{title:"Home"}),i.a.createElement(c.e,{siteTitle:t}))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-349bece098063304fd32.js.map
